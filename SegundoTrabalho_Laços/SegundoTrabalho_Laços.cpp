5df29ec21a2097ac67cf97d2f8af60aaa68f0a46
JibranMac <jibranind31@gmail.com>
2023-10-23 21:57:20 -0300

    Numeros não se repetem Loop dentro de Loop com flag de verificação

--- SegundoTrabalho_Laços/SegundoTrabalho_Laços.cpp
+++ SegundoTrabalho_Laços/SegundoTrabalho_Laços.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
-int main()
-{
-    cout << "Inicio\n";
+int main() {
+
+    srand(time(NULL));
+    const int QTD_SENHA = 5;
+    int senha[QTD_SENHA];
+
+    for (int i = 0; i < QTD_SENHA; i++) {
+        senha[i] = (rand() % 6) + 1;
+        bool flag_repetiu = false;
+        for (int y = i - 1; y >= 0; y--) {
+            if (senha[i] == senha[y]) {
+                flag_repetiu = true;
+            }
+        }
+        if (flag_repetiu == true) {
+            i--;
+        }
+    }
+    for (int i = 0; i < QTD_SENHA; i++) {
+        cout << "Senha[" << i << "]: " << senha[i] << endl;
+    }
+    return 0;
 }
