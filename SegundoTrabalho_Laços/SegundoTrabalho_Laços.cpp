aebdc4e2ac5e2eb37e3bb60db40f605e3a75240d
JibranMac <jibranind31@gmail.com>
2023-10-24 22:44:57 -0300

    Formatação para design

--- SegundoTrabalho_Laços/SegundoTrabalho_Laços.cpp
+++ SegundoTrabalho_Laços/SegundoTrabalho_Laços.cpp
@@ -8,18 +8,20 @@ int main() {
 	int escolha;
 	int dificuldade = 3;
 	int tentativas = 8;
-	string dificuldade_por_escrito = "Facil(Padrao).";
+	string dificuldade_por_escrito = "Facil.    ";
 
 	while (true) {
-
-		cout << "Bem-vindo ao jogo adivinhe a senha!\n";
 		cout << "\n";
-		cout << "1. Fim\n";
-		cout << "2. Sobre\n";
-		cout << "3. Dificuldade: " << dificuldade_por_escrito << endl;
-		cout << "4. Jogar\n";
+		cout << "                           |---------------------------------------------------|\n";
+		cout << "                           |        Bem-vindo ao jogo adivinhe a senha!        |\n";
+		cout << "                           |                                                   |\n";
+		cout << "                           |                    1. Fim                         |\n";
+		cout << "                           |                    2. Sobre                       |\n";
+		cout << "                           |              3. Dificuldade: " << dificuldade_por_escrito << "           |\n";
+		cout << "                           |                    4. Jogar                       |\n";
+		cout << "                           |---------------------------------------------------|\n";
 		cout << "\n";
-		cout << "Escolha: ";
+		cout << "                           | Escolha: ";	
 		cin >> escolha;
 		cout << "\n";
 
@@ -29,15 +31,24 @@ int main() {
 		switch (escolha) {
 
 		case 1://FIM
-			cout << "END GAME!!\n";
+			cout << "\n";
+			cout << "                                           |--------------|\n";
+			cout << "                                           |              |\n";
+			cout << "                                           |   END GAME   |\n";
+			cout << "                                           |              |\n";
+			cout << "                                           |--------------|\n";
+			cout << "\n";
 			return 0;
 
 			break;
 		case 2://SOBRE
-			cout << "Desenvolvimento: Leonardo Jibran Machiavelli.\n";
-			cout << "Data: 23/10/2023\n";
-			cout << "Professor: Eduardo Alves da Silva.\n";
-			cout << "Disciplina: Algoritimos e Programacao.\n";
+			cout << "\n";
+			cout << "                         |---------------------------------------------------|\n";
+			cout << "                         |   Desenvolvimento: Leonardo Jibran Machiavelli.   |\n";
+			cout << "                         |               Data: 23/10/2023                    |\n";
+			cout << "                         |        Professor: Eduardo Alves da Silva.         |\n";
+			cout << "                         |       Disciplina: Algoritimos e Programacao.      |\n";
+			cout << "                         |---------------------------------------------------|\n";
 			cout << "\n";
 
 			system("pause");//pausa o systema ate o usuario digitar uma tecla
@@ -45,25 +56,34 @@ int main() {
 
 			break;
 		case 3://DIFICULDADE
-			cout << "Escolha uma dificuldade: \n";
 			cout << "\n";
-			cout << "1-Facil.\n";
-			cout << "2-Medio.\n";
-			cout << "3-Dificil\n";
+			cout << "                                     |----------------------------|\n";
+			cout << "                                     |  Escolha uma dificuldade:  |\n";
+			cout << "                                     |                            |\n";
+			cout << "                                     |         1-Facil.           |\n";
+			cout << "                                     |         2-Medio.           |\n";
+			cout << "                                     |         3-Dificil.         |\n";
+			cout << "                                     |----------------------------|\n";
 			cout << "\n";
-			cout << "Escolha: ";
+			cout << "                                     | Escolha: ";
 			cin >> escolha;
 			cout << "\n";
 
+			system("pause");//pausa o systema ate o usuario digitar uma tecla
+			system("cls");
+
 			switch (escolha) {
 			case 1://FACIL
-				dificuldade_por_escrito = "Facil.";
-				cout << "Dificuldade do jogo mudou para Facil. \n";
-				cout << "\n";
+				dificuldade_por_escrito = "Facil.    ";
 				dificuldade = 3;
 				tentativas = 8;
-				cout << "Senha: " << dificuldade << " digitos.\n";
-				cout << "Voce possui: " << tentativas << " tentativas.\n";
+				cout << "\n";
+				cout << "                      |-------------------------------------------------|\n";
+				cout << "                      |     Dificuldade do jogo mudou para Facil.       |\n";
+				cout << "                      |                                                 |\n";
+				cout << "                      |               Senha: " << dificuldade << " digitos.                 |\n";
+				cout << "                      |           Voce possui: " << tentativas << " tentativas.            |\n";
+				cout << "                      |-------------------------------------------------|\n";
 				cout << "\n";
 
 				system("pause");//pausa o systema ate o usuario digitar uma tecla
@@ -71,35 +91,46 @@ int main() {
 
 				break;
 			case 2://MEDIO
-				dificuldade_por_escrito = "Medio.";
-				cout << "Dificuldade do jogo mudou para Medio. \n";
-				cout << "\n";
+				dificuldade_por_escrito = "Medio.    ";
 				dificuldade = 4;
 				tentativas = 10;
-				cout << "Senha: " << dificuldade << " digitos.\n";
-				cout << "Voce possui: " << tentativas << " tentativas.\n";
 				cout << "\n";
+				cout << "                      |-------------------------------------------------|\n";
+				cout << "                      |     Dificuldade do jogo mudou para Medio.       |\n";
+				cout << "                      |                                                 |\n";
+				cout << "                      |               Senha: " << dificuldade << " digitos.                 |\n";
+				cout << "                      |           Voce possui: " << tentativas << " tentativas.           |\n";
+				cout << "                      |-------------------------------------------------|\n";
+				cout << "\n";
+
 
 				system("pause");//pausa o systema ate o usuario digitar uma tecla
 				system("cls");
 
 				break;
 			case 3://DIFICIL
-				dificuldade_por_escrito = "Dificil";
-				cout << "Dificuldade do jogo mudou para Facil. \n";
-				cout << "\n";
+				dificuldade_por_escrito = "Dificil.  ";
 				dificuldade = 5;
 				tentativas = 12;
-				cout << "Senha: " << dificuldade << " digitos.\n";
-				cout << "Voce possui: " << tentativas << " tentativas.\n";
 				cout << "\n";
+				cout << "                      |-------------------------------------------------|\n";
+				cout << "                      |     Dificuldade do jogo mudou para Dificil.     |\n";
+				cout << "                      |                                                 |\n";
+				cout << "                      |               Senha: " << dificuldade << " digitos.                 |\n";
+				cout << "                      |           Voce possui: " << tentativas << " tentativas.           |\n";
+				cout << "                      |-------------------------------------------------|\n";
+				cout << "\n";
+
 
 				system("pause");//pausa o systema ate o usuario digitar uma tecla
 				system("cls");
 
 				break;
 			default://NENHUMA_RESPOSTA
-				cout << "Escolha alguma opcao entre: [1 - 3]\n";
+				cout << "\n";
+				cout << "                          |-----------------------------------------|\n";
+				cout << "                          |   Escolha alguma opcao entre: [1 - 3]   |\n";
+				cout << "                          |-----------------------------------------|\n";
 				cout << "\n";
 
 				system("pause");//pausa o systema ate o usuario digitar uma tecla
@@ -111,14 +142,31 @@ int main() {
 		case 4://JOGAR
 
 
+
+
+
+
+
+
+
 			cout << "Vamos la\n";
 
 			system("pause");//pausa o systema ate o usuario digitar uma tecla
 			system("cls");
 
+
+
+
+
+
+
 			break;
 		default://NENHUMA_RESPOSTA
-			cout << "Escolha alguma opcao entre: [1 - 4]\n";
+			cout << "\n";
+			cout << "                          |-----------------------------------------|\n";
+			cout << "                          |   Escolha alguma opcao entre: [1 - 4]   |\n";
+			cout << "                          |-----------------------------------------|\n";
+			cout << "\n";
 
 			system("pause");//pausa o systema ate o usuario digitar uma tecla
 			system("cls");
