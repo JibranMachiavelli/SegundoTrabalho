26b52397efe3819e7389f5326c8997ba560a7a0b
JibranMac <jibranind31@gmail.com>
2023-11-06 19:19:24 -0300

    correção

--- SegundoTrabalho_Laços/SegundoTrabalho_Laços.cpp
+++ SegundoTrabalho_Laços/SegundoTrabalho_Laços.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main() {
 
+	//variaveis
 	int escolha;
 	int dificuldade = 3;
 	int tentativas = 8;
@@ -35,7 +36,7 @@ int main() {
 
 		case 1://FIM
 			for (int r = 0; r < numLines; r++) {
-				cout << "\n";
+				cout << "\n";//Mensagem para mostrar o fim do jogo
 			}
 			cout << "                                                                  |----------------|\n";
 			cout << "                                                                  |                |\n";
@@ -49,7 +50,7 @@ int main() {
 
 			break;
 		case 2://SOBRE
-			cout << "\n";
+			cout << "\n";//Menu para a parte do sobre
 			cout << "                                                |----------------------------------------------------|\n";
 			cout << "                                                |    Disciplina     : Algoritimos e Programacao.     |\n";
 			cout << "                                                |    Professor      : Eduardo Alves da Silva.        |\n";
@@ -64,7 +65,7 @@ int main() {
 
 			break;
 		case 3://DIFICULDADE
-			cout << "\n";
+			cout << "\n";//Menu para escolha de dificuldade
 			cout << "                                                             |--------------------------|\n";
 			cout << "                                                             |  Escolha uma dificuldade:|\n";
 			cout << "                                                             |                          |\n";
@@ -82,7 +83,7 @@ int main() {
 
 			switch (escolha) {
 			case 1://FACIL
-				dificuldade_por_escrito = "Facil.    ";
+				dificuldade_por_escrito = "Facil.    ";//Muda no menu para facil
 				dificuldade = 3;
 				tentativas = 8;
 				cout << "\n";
@@ -99,7 +100,7 @@ int main() {
 
 				break;
 			case 2://MEDIO
-				dificuldade_por_escrito = "Medio.    ";
+				dificuldade_por_escrito = "Medio.    ";//Muda no menu para Medio
 				dificuldade = 4;
 				tentativas = 10;
 				cout << "\n";
@@ -117,7 +118,7 @@ int main() {
 
 				break;
 			case 3://DIFICIL
-				dificuldade_por_escrito = "Dificil.  ";
+				dificuldade_por_escrito = "Dificil.  ";//Muda no menu para dificil
 				dificuldade = 5;
 				tentativas = 12;
 				cout << "\n";
@@ -148,13 +149,13 @@ int main() {
 			}
 			break;
 		case 4://JOGAR
-			srand(static_cast<unsigned int>(time(NULL)));
-			senha = new int[dificuldade];
-
-			for (int i = 0; i < dificuldade; i++) {
-				senha[i] = (rand() % 6) + 1;
+			srand(static_cast<unsigned int>(time(NULL)));//inicializa o gerador de numeros com uma semente baseada no tempo atual
+			senha = new int[dificuldade];//aloca memoria dinamicamente para armazenar a senha com base na dificuldadde
+			//aleatoriza os numeros de 1 a 6
+			for (int i = 0; i < dificuldade; i++) {//percorre cada digito da senha
+				senha[i] = (rand() % 6) + 1;//gera senha aleatoria entre 1 e 6 e atribui a senha[i]
 				bool flag_repetiu = false;
-				for (int y = i - 1; y >= 0; y--) {
+				for (int y = i - 1; y >= 0; y--) {//o loop verifica o numero interno verifica se o numero gerado foi escolhido anteriormente para evitar repetição
 					if (senha[i] == senha[y]) {
 						flag_repetiu = true;
 					}
@@ -179,8 +180,8 @@ int main() {
 			system("pause");
 
 			//tentativa vai diminuindo se os numeros nao forem corretos 
-			for (int i = tentativas; i > 0; i--) {
-				int* escolha = new int[dificuldade];
+			for (int i = tentativas; i > 0; i--) {//percorre as tentativas do jogador
+				int* escolha = new int[dificuldade];//aloca na memoria dinamicamente a escolha.
 				cout << "                                                     |---------------------------------------------------|\n";
 				cout << "                                                     |Tentativas restantes : " << i << "                           |\n";
 				cout << "                                                     |Digite sua tentativa de, " << dificuldade << " numeros (1-6):          |\n";
@@ -188,14 +189,14 @@ int main() {
 				//diiculdade numero de numeros digitados
 				for (int i = 0; i < dificuldade; i++) {
 					cout << "                                                     | Escolha: ";
-					cin >> escolha[i];
+					cin >> escolha[i];//sua tentativa é alocada aqui
 					//cout << " vetor [" << i << "]" << " senha :" << senha[i] << endl;
 
 				}
 				int posição_certa = 0;
 				int posição_errada = 0;
 
-				for (int i = 0; i < dificuldade; i++) {
+				for (int i = 0; i < dificuldade; i++) {//Loop vai rodar  para verificar a escolha 
 					for (int j = 0; j < dificuldade; j++) {
 						if (escolha[i] == senha[j]) {
 							if (i == j) {
@@ -217,25 +218,23 @@ int main() {
 				//system("pause");
 
 				//Mostra se voce ganhou ou perdeu(jogo nao para)
-				if (posição_certa == dificuldade) {
+				if (posição_certa == dificuldade) {//Mostra se voce acertou todos os numeros
 					cout << "                                                    |----------------------------------------------------|\n";
 					cout << "                                                    |          Voce acertou a senha parabens!!!!         |       " << endl;
 					cout << "                                                    |----------------------------------------------------|\n";
 					break;
 
 				}
-				if (i == 1) {
+				if (i == 1) { //se o contador chegar a 1 mostra que perdeu!
 					cout << "                                                     |--------------------------------------------------|\n";
 					cout << "                                                     |             Infelizmente voce perdeu!!           |" << endl;
 					cout << "                                                     |             A senha era :                        |" << endl;
 					cout << "                                                     |--------------------------------------------------|\n";
-					for (int i = 0; i < dificuldade; i++) {
+					for (int i = 0; i < dificuldade; i++) {//Mostra a senha
 						cout << "                                                     |" << i + 1 << " - Senha : " << senha[i] << endl;
 					}
 				}
 			}
-
-
 
 			system("pause");//pausa o systema ate o usuario digitar uma tecla
 			system("cls");
@@ -252,9 +251,8 @@ int main() {
 			system("cls");
 
 			break;
-
 		}
 	}
-	delete[]senha;
+	delete[]senha;//Deletar da memoria
 	return 0;
 }
